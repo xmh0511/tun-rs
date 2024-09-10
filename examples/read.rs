09309b20119d53793aff82aab475c8ca44f84966
xmh0511 <970252187@qq.com>
2024-09-10 20:54:25 +0800

    update

--- examples/read.rs
+++ examples/read.rs
@@ -42,13 +42,13 @@ fn main_entry(quit: Receiver<()>) -> Result<(), BoxError> {
     // config.layer(Layer::L2);
 
     config
-        // .address_with_prefix(
-        //     "CDCD:910A:2222:5498:8475:1111:3900:2020"
-        //         .parse::<IpAddr>()
-        //         .unwrap(),
-        //     "ffff:ffff:ffff:ffff::".parse::<IpAddr>().unwrap(),
-        // )
-        .address_with_prefix((10, 0, 0, 9), 24u8)
+        .address_with_prefix(
+            "CDCD:910A:2222:5498:8475:1111:3900:2020"
+                .parse::<IpAddr>()
+                .unwrap(),
+            "ffff:ffff:ffff:ffff::".parse::<IpAddr>().unwrap(),
+        )
+        //.address_with_prefix((10, 0, 0, 9), 24u8)
         .destination((10, 0, 0, 1))
         .up();
 
@@ -63,7 +63,7 @@ fn main_entry(quit: Receiver<()>) -> Result<(), BoxError> {
         #[allow(unreachable_code)]
         Ok::<(), BoxError>(())
     });
-    dev_t.set_network_address((10, 0, 0, 88), (255, 255, 255, 0), None)?;
+    //dev_t.set_network_address((10, 0, 0, 88), (255, 255, 255, 0), None)?;
     // dev_t.set_network_address(
     //     "CDCD:910A:2222:5498:8475:1111:3900:2024"
     //         .parse::<IpAddr>()
