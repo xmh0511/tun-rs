eba7bfe07494bad2c6ce0acdb754c3f98d8f7a9f
xmh0511 <970252187@qq.com>
2024-09-10 20:52:01 +0800

    update

--- src/platform/freebsd/device.rs
+++ src/platform/freebsd/device.rs
@@ -384,7 +384,7 @@ impl AbstractDevice for Device {
         let ifs = addrs
             .filter(|v| v.name == if_name)
             .collect::<Vec<Interface>>();
-        if let Some(v) = ifs.last() {
+        if let Some(v) = ifs.first() {
             return Ok(v.address);
         }
         Err(Error::String("AddrNotAvailable".to_string()))
@@ -396,7 +396,7 @@ impl AbstractDevice for Device {
         let ifs = addrs
             .filter(|v| v.name == if_name)
             .collect::<Vec<Interface>>();
-        if let Some(v) = ifs.last() {
+        if let Some(v) = ifs.first() {
             return v
                 .dest_addr
                 .ok_or(Error::String("DestAddrNotAvailable".to_string()));
@@ -425,7 +425,7 @@ impl AbstractDevice for Device {
         let ifs = addrs
             .filter(|v| v.name == if_name)
             .collect::<Vec<Interface>>();
-        if let Some(v) = ifs.last() {
+        if let Some(v) = ifs.first() {
             return v
                 .netmask
                 .ok_or(Error::String("NetMaskNotAvailable".to_string()));
