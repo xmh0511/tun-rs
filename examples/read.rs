51bb3cbc395d9c32267dc7bff412f94f660a9acc
xmh0511 <970252187@qq.com>
2024-09-12 14:17:15 +0800

    u

--- examples/read.rs
+++ examples/read.rs
@@ -53,8 +53,6 @@ fn main_entry(quit: Receiver<()>) -> Result<(), BoxError> {
         .up();
 
     let dev = Arc::new(tun_rs::create(&config)?);
-	#[cfg(any(target_os = "linux", target_os = "freebsd",))]
-	dev.set_broadcast((10,0,0,9))?;
     let dev_t = dev.clone();
     let _join = std::thread::spawn(move || {
         let mut buf = [0; 4096];
