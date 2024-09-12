054028ec571f20e5d2c4442f2d361251a163bf94
xmh0511 <970252187@qq.com>
2024-09-12 21:24:00 +0800

    update

--- examples/read.rs
+++ examples/read.rs
@@ -38,17 +38,17 @@ fn main_entry(quit: Receiver<()>) -> Result<(), BoxError> {
 
     let mut config = tun_rs::Configuration::default();
 
-    #[cfg(any(target_os = "windows", target_os = "linux", target_os = "freebsd",))]
-    config.layer(Layer::L2);
+    // #[cfg(any(target_os = "windows", target_os = "linux", target_os = "freebsd",))]
+    // config.layer(Layer::L2);
 
     config
-        // .address_with_prefix(
-        //     "CDCD:910A:2222:5498:8475:1111:3900:2020"
-        //         .parse::<IpAddr>()
-        //         .unwrap(),
-        //     64,
-        // )
-        .address_with_prefix((10, 0, 0, 9), 24u8)
+        .address_with_prefix(
+            "CDCD:910A:2222:5498:8475:1111:3900:2020"
+                .parse::<IpAddr>()
+                .unwrap(),
+            64,
+        )
+        //.address_with_prefix((10, 0, 0, 2), 24u8)
         //.destination((10, 0, 0, 1))
         .up();
 
