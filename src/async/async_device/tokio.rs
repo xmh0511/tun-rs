92426438fd1c27f44d75dd51ce96f8d8a7bd44e9
xmh0511 <970252187@qq.com>
2024-12-23 16:11:49 +0800

    implement AsyncRead/AsyncWrite for unix with tokio

--- src/async/async_device/tokio.rs
+++ src/async/async_device/tokio.rs
@@ -2,7 +2,10 @@ use crate::platform::Device;
 use ::tokio::io::unix::AsyncFd as TokioAsyncFd;
 use ::tokio::io::Interest;
 use std::io;
-use std::io::IoSlice;
+use std::io::{Error, IoSlice};
+use std::pin::Pin;
+use std::task::{ready, Context, Poll};
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 
 pub struct AsyncFd(TokioAsyncFd<Device>);
 impl AsyncFd {
@@ -30,5 +33,53 @@ impl AsyncFd {
 
     pub fn get_ref(&self) -> &Device {
         self.0.get_ref()
+    }
+}
+
+impl AsyncRead for AsyncFd {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        loop {
+            let mut guard = ready!(self.0.poll_read_ready(cx))?;
+            let unfilled = buf.initialize_unfilled();
+            match guard.try_io(|inner| inner.get_ref().tun.recv(unfilled)) {
+                Ok(Ok(len)) => {
+                    buf.advance(len);
+                    return Poll::Ready(Ok(()));
+                }
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+}
+
+impl AsyncWrite for AsyncFd {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, Error>> {
+        loop {
+            let mut guard = ready!(self.0.poll_write_ready(cx))?;
+
+            match guard.try_io(|inner| inner.get_ref().tun.send(buf)) {
+                Ok(result) => return Poll::Ready(result),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        #[cfg(feature = "experimental")]
+        self.0.get_ref().tun.shutdown()?;
+        Poll::Ready(Ok(()))
     }
 }
